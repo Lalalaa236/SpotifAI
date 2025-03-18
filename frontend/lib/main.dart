88375425a5a03285dae96af7e4e8ab9541e1ed4f
Lê Nguyễn Minh Hiếu <prohieu2004@gmail.com>
2025-03-18 23:17:01 +0700

    update: Temporary Login Screen and Updated Folder Structure

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'github_oauth_credentials.dart';
-import 'src/github_login.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,33 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GitHub Client',
+      debugShowCheckedModeBanner: false,
+      title: 'Spotify Login',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         visualDensity: VisualDensity.adaptivePlatformDensity,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'GitHub Client'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return GithubLoginWidget(
-      builder: (context, httpClient) {
-        return Scaffold(
-          appBar: AppBar(title: Text(title), elevation: 2),
-          body: const Center(child: Text('You are logged in to GitHub!')),
-        );
-      },
-      githubClientId: githubClientId,
-      githubClientSecret: githubClientSecret,
-      githubScopes: githubScopes,
+      home: const LoginScreen(),
     );
   }
 }
